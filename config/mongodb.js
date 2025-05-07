7fa5c7a9d8d526ae4b9bb1db40014b2d6405df3e
Kajanthan <kajanthan2k1@gmail.com>
2025-05-08 03:13:43 +0530

    changes

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connectDB = async () =>{
+const connectDB = async () => {
+  mongoose.connection.on('connected', () => {
+    console.log('✅ MongoDB is connected');
+  });
 
-    mongoose.connection.on('connected', () => {
-        console.log('MongoDB is connected');
-    })
-    await mongoose.connect(`${process.env.MONGODB_URL}/countries`);
-    
-}
+  console.log("MONGODB_URL:", process.env.MONGODB_URL); // Debug line
 
-export default connectDB
+  await mongoose.connect(`${process.env.MONGODB_URL}`);
+};
+
+export default connectDB;
